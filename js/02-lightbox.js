ca0eb10fc17b95115166f92886b8d8e651c2d02e
romanhalaichuk <vi2virbien@gmail.com>
2023-11-09 13:28:11 +0100

    EE

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+//! Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryCardsSet = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+	return galleryItems
+		.map(({ original, preview, description }) => {
+			return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+</div>`;
+		})
+		.join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryCardsSet);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+	caption: true,
+	captionsData: "alt",
+	captionDelay: 250,
+});
